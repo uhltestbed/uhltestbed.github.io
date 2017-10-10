b26565a6febff180480ccfecc305e364f6332f15
uhltestbed <32659201+uhltestbed@users.noreply.github.com>
2017-10-09 19:29:16 -0700

    Update getdb.js

--- getdb.js
+++ getdb.js
@@ -1,3 +1,5 @@
+<script type="text/javascript">
+
 function init() {
  Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1acfPFGKbL8C_c19xyuQ3aHrq7iTI1kjzw5yyPVnNUcs/pubhtml',
  callback: function(data, tabletop) { 
@@ -6,3 +8,5 @@ function init() {
  simpleSheet: true } )
 }
 window.addEventListener(‘DOMContentLoaded’, init)
+
+</script>
